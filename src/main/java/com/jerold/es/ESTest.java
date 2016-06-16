485827ca4254489a7ca9186dfb9d7d171cb7c117
jeroldjwl <1017794273@qq.com>
2016-06-16 21:40:53 +0800

    commit at 06/16 21:40

--- src/main/java/com/jerold/es/ESTest.java
+++ src/main/java/com/jerold/es/ESTest.java
@@ -4,8 +4,8 @@ package com.jerold.es;
  * Created by Jerold on 16/6/16.
  */
 public class ESTest {
-  public static void main(String[] args)
-  {
-      System.out.println("this is test");
-  }
+    public static void main(String[] args) {
+        System.out.println("this is test");
+        System.out.println("test push");
+    }
 }
---
+++ src/main/java/com/jerold/util/UtilTest.java
@@ -0,0 +1,10 @@
+package com.jerold.util;
+
+/**
+ * Created by Jerold on 16/6/16.
+ */
+public class UtilTest {
+    public static void main(String[] args) {
+        System.out.println("util test");
+    }
+}
